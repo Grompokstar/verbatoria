9afe3d27c4abf54e62055f48baac4b29ba4d492f
Grompokstar <roma.chervontsev@gmail.com>
2017-06-03 14:42:34 +0300

    main menu

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function toggleVisibilityMainMenu() {
+    $('.main-menu').toggleClass('visible');
+    $('.main-menu-btn').toggleClass('close');
+    $('.main-menu-btn svg').toggleClass('visible');
+    $('.overlay').toggleClass('visible');
+}
